6a29e429b5b870480e590ecb2b3bb23bcdb92efb
work <helloheresin@gmail.com>
2018-08-25 17:05:46 +0800

    test
    Signed-off-by: work <helloheresin@gmail.com>

---
+++ application/index/model/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *     __ __                _  __ _
+ *    / // /___  ____ ___  / |/ /(_)___  ___ _
+ *   / _  // -_)/ __// -_)/    // // _ \/ _ `/
+ *  /_//_/ \__//_/   \__//_/|_//_//_//_/\_, /
+ *                                     /___/
+ * User: HereNing
+ * Date: 2018/8/25
+ * Time: 14:35
+ */
+
+namespace app\index\model;
+
+use think\Model;
+
+class Test extends Model{
+    protected $autoWriteTimestamp = true;
+}
