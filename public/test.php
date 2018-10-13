c6b03bae8ec8c19a557ed78923ddea68578bfa0d
work <helloheresin@gmail.com>
2018-10-13 11:37:13 +0800

    del database.php

---
+++ public/test.php
@@ -0,0 +1,53 @@
+<?php
+
+/*class Demo{
+
+    final public function test(){
+        echo '111';
+    }
+}
+
+
+class Newbee extends Demo{
+    public function index(){
+        echo 2222;
+    }
+
+}
+
+
+$a = new Newbee();
+echo $a ->index();*/
+
+/*class SimpleClass
+{
+    public $var = 'test';
+}
+
+$instance = new SimpleClass();
+
+$assigned   =  $instance;
+$reference  = &$instance;
+
+$instance->var = '$assigned will have this value';
+
+
+//$instance = null; // $instance and $reference become null
+$reference = null;
+
+
+var_dump($instance);
+var_dump($reference);
+var_dump($assigned);*/
+
+
+function foo($var)
+{
+    return $var++;
+}
+
+$a = 5;
+$b = foo($a);
+
+var_dump($a);
+var_dump($b);
