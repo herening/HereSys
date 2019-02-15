166da426242fea2a17787f8041309abbabf0f441
work <helloheresin@gmail.com>
2019-02-15 11:07:59 +0800

    222

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -28,10 +28,6 @@ class Index extends AdminBase
 
     public function Login()
     {
-        if($this->is_login()){
-            $this->error('you have logged in','admin/index/index');
-            //return ['code' => 0, 'msg' => translate('you have logged in')];
-        }
         if($this->request->isPost()){
             $data = input('post.');
             $validate = $this->validate($data, 'app\admin\validate\AdminUser');
